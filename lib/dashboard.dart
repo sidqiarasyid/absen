7b4f1f58a014df7176ec84b20c5e49b007176900
sidqi <sidqiarasyid31@gmail.com>
2022-09-12 13:19:11 +0700

    feat: [SA] ui dashboard, absen, dan selesai absen

---
+++ lib/dashboard.dart
@@ -0,0 +1,62 @@
+import 'package:absen/absen_masuk.dart';
+import 'package:absen/absen_pulang.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class DashboardPage extends StatefulWidget {
+  const DashboardPage({Key? key}) : super(key: key);
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 18),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Absen", style: GoogleFonts.roboto(fontWeight: FontWeight.w500, fontSize: 18),),
+                IconButton(onPressed: (){}, icon: Icon(Icons.person))
+              ],
+            ),
+          ),
+          SizedBox(height: 65,),
+          Container(
+              width: 340,
+              height: 45,
+              child: OutlinedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => AbsenMasukPage(),));
+                },
+                child: Text("Absen Masuk", ),
+                style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Colors.blue),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),)),
+              )),
+          SizedBox(height: 18,),
+          Container(
+              width: 340,
+              height: 45,
+              child: OutlinedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => AbsenPulangPage(),));
+                },
+                child: Text("Absen Pulang", ),
+                style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Colors.blue),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),)),
+              )),
+        ],
+      ),
+    ));
+  }
+}
--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,4 @@
+import 'package:absen/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -18,9 +19,18 @@ class _LoginPageState extends State<LoginPage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(height: 100,),
-              Text("Absen Guru RPL \n SMK RUS",textAlign: TextAlign.center, style: GoogleFonts.roboto(fontSize: 32, fontWeight: FontWeight.w500),),
-              SizedBox(height: 80,),
+              SizedBox(
+                height: 100,
+              ),
+              Text(
+                "Absen Guru RPL \n SMK RUS",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.roboto(
+                    fontSize: 32, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: 80,
+              ),
               Container(
                 height: 180,
                 child: Image.asset(
@@ -28,36 +38,57 @@ class _LoginPageState extends State<LoginPage> {
                   fit: BoxFit.cover,
                 ),
               ),
-              SizedBox(height: 100,),
+              SizedBox(
+                height: 100,
+              ),
               Container(
                 padding: EdgeInsets.symmetric(horizontal: 35),
                 child: TextField(
                   decoration: InputDecoration(
                       labelText: "Nomor induk yayasan",
-                      labelStyle: GoogleFonts.roboto(fontWeight: FontWeight.w300, fontSize: 14),
-                      prefixIcon: Icon(Icons.person_outline, color: Colors.black,)
-                  ),
+                      labelStyle: GoogleFonts.roboto(
+                          fontWeight: FontWeight.w300, fontSize: 14),
+                      prefixIcon: Icon(
+                        Icons.person_outline,
+                        color: Colors.black,
+                      )),
                 ),
               ),
-              SizedBox(height: 30,),
+              SizedBox(
+                height: 30,
+              ),
               Container(
                 padding: EdgeInsets.symmetric(horizontal: 35),
                 child: TextField(
                   decoration: InputDecoration(
                       labelText: "Password",
-                      labelStyle: GoogleFonts.roboto(fontWeight: FontWeight.w300, fontSize: 14),
-                      prefixIcon: Icon(Icons.lock_outline, color: Colors.black,)
-                  ),
+                      labelStyle: GoogleFonts.roboto(
+                          fontWeight: FontWeight.w300, fontSize: 14),
+                      prefixIcon: Icon(
+                        Icons.lock_outline,
+                        color: Colors.black,
+                      )),
                 ),
               ),
-              SizedBox(height: 50,),
-              Container(width: 310 ,child: ElevatedButton(onPressed: (){}, child: Text("Login"), style: ElevatedButton.styleFrom(
-                  primary: Colors.blue, shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16)
-              )
-              ),)),
-
-
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                  width: 310,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DashboardPage(),
+                          ));
+                    },
+                    child: Text("Login"),
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16))),
+                  )),
             ],
           ),
         ),
